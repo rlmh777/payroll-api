ae56443a1f70ee82cc460d45fce898cdaccfc864
Luis Herrera <bzpointer@gmail.com>
2025-03-23 14:43:33 -0600

    update contact type model

--- app/Models/ContactType.php
+++ app/Models/ContactType.php
@@ -3,13 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class ContactType extends Model
 {
+    use HasUuids;
+
     protected $table = 'contact_type';
-    protected $primarykey = 'id';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
 
     protected $fillable = [
-        'name'
+        'name',
+        'description',
+        'is_active'
     ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+
+    /**
+     * Get the contacts for this type.
+     */
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
 }
