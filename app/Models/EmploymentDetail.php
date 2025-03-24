4cb0ab7ec38bd2a31498eb8082a2c628c8cbbbc5
Imer Lopez <imerbyte@gmail.com>
2025-03-23 23:15:16 -0600

    Updated Model to address foreign key issue. Added Seeder. Feat Pass QA.

--- app/Models/EmploymentDetail.php
+++ app/Models/EmploymentDetail.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Relation\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmploymentDetail extends Model
 {
@@ -34,37 +34,45 @@ class EmploymentDetail extends Model
         'worksiteId'
     ];
 
-    public function employee(): BelongsTo {
+    public function employee(): BelongsTo
+    {
         return $this->belongsTo(Employee::class);
     }
-    
-    public function payrateFrequency(): BelongsTo {
+
+    public function payrateFrequency(): BelongsTo
+    {
         return $this->belongsTo(PayrateFrequency::class);
     }
 
-    public function chartOfAccount(): BelongsTo {
+    public function chartOfAccount(): BelongsTo
+    {
         return $this->belongsTo(ChartOfAccount::class);
     }
 
-    public function contractType(): BelongsTo {
+    public function contractType(): BelongsTo
+    {
         return $this->belongsTo(ContractType::class);
     }
 
-    public function employmentStatus(): BelongsTo {
+    public function employmentStatus(): BelongsTo
+    {
         return $this->belongsTo(EmploymentStatus::class);
     }
 
-    public function department(): BelongsTo {
+    public function department(): BelongsTo
+    {
         return $this->belongsTo(Department::class);
     }
 
-    public function worksite(): BelongsTo {
+    public function worksite(): BelongsTo
+    {
         return $this->belongsTo(Worksite::class);
     }
 
-    public function employeeStatus(): BelongsTo {
+    public function employeeStatus(): BelongsTo
+    {
         return $this->belongsTo(EmployeeStatus::class);
     }
 
-    
+
 }
