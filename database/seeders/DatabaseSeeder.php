a9244c2cfc9d94dedad9710502e214a7b6ac6fbe
Luis Herrera <bzpointer@gmail.com>
2025-03-30 21:30:44 -0600

    update db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,6 +35,7 @@ class DatabaseSeeder extends Seeder
             RoleAndPermissionSeeder::class,
             DegreeSeeder::class,
             EmployeeStatusSeeder::class,
+            RelationshipSeeder::class,
         ]);
     }
 }
