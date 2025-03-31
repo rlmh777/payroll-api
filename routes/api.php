62b9cd327057fa599bb1cd2d8ec18545394f290d
Luis Herrera <bzpointer@gmail.com>
2025-03-30 22:54:40 -0600

    handle loan endpoints

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\EmployeeDefaultDeductionController;
 use App\Http\Controllers\EmploymentDetailsController;
 use App\Http\Controllers\EmployeeHoursWorkedController;
+use App\Http\Controllers\EmploymentHistoryController;
+use App\Http\Controllers\LoanController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -168,4 +170,22 @@ Route::prefix('employee-hours-worked')->group(function () {
     Route::get('/{hoursWorked}', [EmployeeHoursWorkedController::class, 'show']);
     Route::put('/{hoursWorked}', [EmployeeHoursWorkedController::class, 'update']);
     Route::delete('/{hoursWorked}', [EmployeeHoursWorkedController::class, 'destroy']);
+});
+
+// Employment History Routes
+Route::prefix('employment-histories')->group(function () {
+    Route::get('/', [EmploymentHistoryController::class, 'index']);
+    Route::post('/', [EmploymentHistoryController::class, 'store']);
+    Route::get('/{employmentHistory}', [EmploymentHistoryController::class, 'show']);
+    Route::put('/{employmentHistory}', [EmploymentHistoryController::class, 'update']);
+    Route::delete('/{employmentHistory}', [EmploymentHistoryController::class, 'destroy']);
+});
+
+// Loan Routes
+Route::prefix('loans')->group(function () {
+    Route::get('/', [LoanController::class, 'index']);
+    Route::post('/', [LoanController::class, 'store']);
+    Route::get('/{loan}', [LoanController::class, 'show']);
+    Route::put('/{loan}', [LoanController::class, 'update']);
+    Route::delete('/{loan}', [LoanController::class, 'destroy']);
 });
